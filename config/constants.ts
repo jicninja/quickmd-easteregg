2110c5babf91a8cd5a1bf53abbcef5b713157bd7
Ignacio <ignacio.castro@utility.agency>
2024-11-08 15:41:31 -0300

    update online server

--- config/constants.ts
+++ config/constants.ts
@@ -1,6 +1,6 @@
 import type { TileProps } from '../types/tiles';
 
-const API_DOMAIN = 'http://192.168.68.81:3000/';
+const API_DOMAIN = 'https://quickmd-server.onrender.com/'; //'http://192.168.68.81:3000/';
 const TILE_SIZE = 64;
 
 const EMPTY_MATRIX: TileProps[][] | {}[][] = [
